a91340d92d4f450f4361a8ba0cd4bcea3712812f
me-safty <erensafty@gmail.com>
2023-09-08 00:29:40 +0300

    updete job alert

--- jobAlert.js
+++ jobAlert.js
@@ -1,10 +1,10 @@
-// const audio = new Audio(
-//   "https://cms-artifacts.motionarray.com/content/motion-array/1068814/Hip_Hop_Sound_Elements_mp3.mp3?Expires=2003734661106&Key-Pair-Id=K2ZDLYDZI2R1DF&Signature=VfhsVwLl9ZkHLOuGaraPFUT-5l-Z0-sV1BcyjON5yja8CjbmE4olL9NrODOfHIC0i5sgzo-hA59Qlf-4D6on~nGLfe4mBfBIHUpB66FF3YgaF6Rxylu4E6KfQn6gAXUR9oMw~Z97qt9~jQb38E4BUogxnBIaGxnEalZIZeBpsJVrhvxbTIdyl4I8t342DxOXhihPMpwQ6S5hi-xN3JlzMwO9zAIrWGD0uP2GEUC~82SxmleEKIVnOk2Q2FsXvFmYmAA-zrjIjHerIM8wrKU8uU2xf0OHRi2IIb5vxbZFroXl9yb5xPr0wea8jlMkaXrury5ZsPuCKZSi5mPCJYGE8A__"
-// )
+const audio = new Audio(
+  "https://cms-artifacts.motionarray.com/content/motion-array/1068814/Hip_Hop_Sound_Elements_mp3.mp3?Expires=2003734661106&Key-Pair-Id=K2ZDLYDZI2R1DF&Signature=VfhsVwLl9ZkHLOuGaraPFUT-5l-Z0-sV1BcyjON5yja8CjbmE4olL9NrODOfHIC0i5sgzo-hA59Qlf-4D6on~nGLfe4mBfBIHUpB66FF3YgaF6Rxylu4E6KfQn6gAXUR9oMw~Z97qt9~jQb38E4BUogxnBIaGxnEalZIZeBpsJVrhvxbTIdyl4I8t342DxOXhihPMpwQ6S5hi-xN3JlzMwO9zAIrWGD0uP2GEUC~82SxmleEKIVnOk2Q2FsXvFmYmAA-zrjIjHerIM8wrKU8uU2xf0OHRi2IIb5vxbZFroXl9yb5xPr0wea8jlMkaXrury5ZsPuCKZSi5mPCJYGE8A__"
+)
 
-// const comeToMe = new Audio(
-//   "https://cms-artifacts.motionarray.com/content/motion-array/145884/Devil_Says_Come_To_Me_mp3.mp3?Expires=2003734653332&Key-Pair-Id=K2ZDLYDZI2R1DF&Signature=xLaxikYP7bYpm3o9CxxHvw2HkDbl2AzrD29YjQjDgTr1-594jhXkFTX~PV8aDyp9ywAN3dsXmY2Vkcy1BFbr0m6M4B~o3Kh4vC2Sxy~7VtPRMMCIj2axDZoDcNLUfu15XfWL-Nnc31j-cpTrNwKtwSKbOEOO7b2H7ZwMfNfm330tjaMe3VwRYOiDUWEqgcGSOI~0FgioZaEVDJxgapFVwqAk5WPBN~lDcvSzAWRKcvnq0alwh0~E~8VvCfX9WWYor~RyJ2fdUrXiCXsznlwbFp9GGilAf9hyhpVYm7sEG7bYoeV8MwWgokXaNqxRPimqmenhn8KnkTpRYkh1yw3nHg__"
-// )
+const comeToMe = new Audio(
+  "https://cms-artifacts.motionarray.com/content/motion-array/145884/Devil_Says_Come_To_Me_mp3.mp3?Expires=2003734653332&Key-Pair-Id=K2ZDLYDZI2R1DF&Signature=xLaxikYP7bYpm3o9CxxHvw2HkDbl2AzrD29YjQjDgTr1-594jhXkFTX~PV8aDyp9ywAN3dsXmY2Vkcy1BFbr0m6M4B~o3Kh4vC2Sxy~7VtPRMMCIj2axDZoDcNLUfu15XfWL-Nnc31j-cpTrNwKtwSKbOEOO7b2H7ZwMfNfm330tjaMe3VwRYOiDUWEqgcGSOI~0FgioZaEVDJxgapFVwqAk5WPBN~lDcvSzAWRKcvnq0alwh0~E~8VvCfX9WWYor~RyJ2fdUrXiCXsznlwbFp9GGilAf9hyhpVYm7sEG7bYoeV8MwWgokXaNqxRPimqmenhn8KnkTpRYkh1yw3nHg__"
+)
 
 let job = document.querySelector(
   ".up-card-section.up-card-list-section.up-card-hover"
