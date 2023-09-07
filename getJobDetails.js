099571691e8c43022425959997cbce2c12b67044
me-safty <erensafty@gmail.com>
2023-09-08 00:28:22 +0300

    updete job alert

--- getJobDetails.js
+++ getJobDetails.js
@@ -6,13 +6,13 @@
 //   "https://cms-artifacts.motionarray.com/content/motion-array/145884/Devil_Says_Come_To_Me_mp3.mp3?Expires=2003734653332&Key-Pair-Id=K2ZDLYDZI2R1DF&Signature=xLaxikYP7bYpm3o9CxxHvw2HkDbl2AzrD29YjQjDgTr1-594jhXkFTX~PV8aDyp9ywAN3dsXmY2Vkcy1BFbr0m6M4B~o3Kh4vC2Sxy~7VtPRMMCIj2axDZoDcNLUfu15XfWL-Nnc31j-cpTrNwKtwSKbOEOO7b2H7ZwMfNfm330tjaMe3VwRYOiDUWEqgcGSOI~0FgioZaEVDJxgapFVwqAk5WPBN~lDcvSzAWRKcvnq0alwh0~E~8VvCfX9WWYor~RyJ2fdUrXiCXsznlwbFp9GGilAf9hyhpVYm7sEG7bYoeV8MwWgokXaNqxRPimqmenhn8KnkTpRYkh1yw3nHg__"
 // )
 
-// "https://cms-artifacts.motionarray.com/content/motion-array/1297037/Knock_At_The_Door_mp3.mp3?Expires=2003734670657&Key-Pair-Id=K2ZDLYDZI2R1DF&Signature=gFm01Y~XUy5ZaV9Qbb1NkNAxBqw~W41yAd7pkZ3T4hrH64-eRgG2n2nLAvHfRZtHh32yp6uvCBZ7TMFk8-k0UJE47qfFc0vH4mmK23R9IWyp8BlXoklUvNmuIEHgjewGZ1eunytqu8bUtkyRtiUdQhICsz4CVLiAwnBeb4jvqfhk2qzwNZLLV57IrupVx2Ql5FRddSYjFxAV8T3Ibw75Ci1leB8DqAcDpLtsY~A57LGkUKypS2SThxNu1ks0Iccl87h0TJolYOYZqhvN7FCBNaKGDRnZLWq8tDYFmi2yEcmi7JO1Bw8-O7VhLwhV2r45104bdTykdL2Bk64AWx3iFQ__"
 let job = document.querySelector(
   ".up-card-section.up-card-list-section.up-card-hover"
 )
 
 const knockDoor = new Audio(
-  "..." // audio url
+  // audio url
+  "https://cms-artifacts.motionarray.com/content/motion-array/1297037/Knock_At_The_Door_mp3.mp3?Expires=2003734670657&Key-Pair-Id=K2ZDLYDZI2R1DF&Signature=gFm01Y~XUy5ZaV9Qbb1NkNAxBqw~W41yAd7pkZ3T4hrH64-eRgG2n2nLAvHfRZtHh32yp6uvCBZ7TMFk8-k0UJE47qfFc0vH4mmK23R9IWyp8BlXoklUvNmuIEHgjewGZ1eunytqu8bUtkyRtiUdQhICsz4CVLiAwnBeb4jvqfhk2qzwNZLLV57IrupVx2Ql5FRddSYjFxAV8T3Ibw75Ci1leB8DqAcDpLtsY~A57LGkUKypS2SThxNu1ks0Iccl87h0TJolYOYZqhvN7FCBNaKGDRnZLWq8tDYFmi2yEcmi7JO1Bw8-O7VhLwhV2r45104bdTykdL2Bk64AWx3iFQ__"
 )
 
 setInterval(() => {
